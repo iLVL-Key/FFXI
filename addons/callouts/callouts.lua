135b2264fa8c156adfe7059053fec62ceddd61e6
iLVL-Key <101156258+iLVL-Key@users.noreply.github.com>
2023-01-12 23:43:41 -0600

    01.12.23
    -Fixed Paralyze issue
    -Added callouts for Bumba 1-HR moves.

--- addons/callouts/callouts.lua
+++ addons/callouts/callouts.lua
@@ -25,18 +25,18 @@
 --SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 _addon.name = 'Callouts'
-_addon.version = '01.10.23'
+_addon.version = '01.12.23'
 _addon.author = 'Key'
 _addon.commands = {'callouts','co'}
 
 require 'logger'
 res = require 'resources'
 local chat = windower.chat.input
-local command = windower.send_command
+local cmd = windower.send_command
 
 
 --Default Chat mode on load.
-chatmode = 'echo'
+chatmode = 'p'
 --Party (p) and Echo (echo) are the two that can be toggled between via command but any can be used for default (careful!)
 
 
@@ -48,46 +48,61 @@ chatmode = 'echo'
 windower.register_event('action',function(act)
 
 	local actor = windower.ffxi.get_mob_by_id(act.actor_id)
-	local targets = act.targets
 	local target_name = windower.ffxi.get_mob_by_id(act.targets[1].id).name or 'Unknown'
 
-	if act.category == 7 and not act.param == 28787 then --28787 indicates failure (paralyzed)
+	if act.category == 7 and not act.category == nil then
+
 		if actor.name == "Aita" or actor.name == "Degei" then
-			if res.monster_abilities[targets[1].actions[1].param].en == 'Flaming Kick' then 
+			if res.monster_abilities[act.targets[1].actions[1].param].en == 'Flaming Kick' then 
 				chat('/'..chatmode..' Water (Flaming Kick) <call14>')
-			elseif res.monster_abilities[targets[1].actions[1].param].en == 'Flashflood' then 
+			elseif res.monster_abilities[act.targets[1].actions[1].param].en == 'Flashflood' then 
 				chat('/'..chatmode..' Thunder (Flashflood) <call14>')
-			elseif res.monster_abilities[targets[1].actions[1].param].en == 'Icy Grasp' then 
+			elseif res.monster_abilities[act.targets[1].actions[1].param].en == 'Icy Grasp' then 
 				chat('/'..chatmode..' Fire (Icy Grasp) <call14>')
-			elseif res.monster_abilities[targets[1].actions[1].param].en == 'Eroding Flesh' then 
+			elseif res.monster_abilities[act.targets[1].actions[1].param].en == 'Eroding Flesh' then 
 				chat('/'..chatmode..' Aero (Eroding Flesh) <call14>')
-			elseif res.monster_abilities[targets[1].actions[1].param].en == 'Fulminous Smash' then 
+			elseif res.monster_abilities[act.targets[1].actions[1].param].en == 'Fulminous Smash' then 
 				chat('/'..chatmode..' Stone (Fulminous Smash) <call14>')
 			end
 
 		elseif actor.name == "Aminon" then
-			if res.monster_abilities[targets[1].actions[1].param].en == 'Demonfire' then 
+			if res.monster_abilities[act.targets[1].actions[1].param].en == 'Demonfire' then 
 				chat('/'..chatmode..' Water (Demon Fire) <call14>')
-			elseif res.monster_abilities[targets[1].actions[1].param].en == 'Torrential Pain' then 
+			elseif res.monster_abilities[act.targets[1].actions[1].param].en == 'Torrential Pain' then 
 				chat('/'..chatmode..' Thunder (Torrential Pain) <call14>')
-			elseif res.monster_abilities[targets[1].actions[1].param].en == 'Frozen Blood' then 
+			elseif res.monster_abilities[act.targets[1].actions[1].param].en == 'Frozen Blood' then 
 				chat('/'..chatmode..' Fire (Frozen Blood) <call14>')
-			elseif res.monster_abilities[targets[1].actions[1].param].en == 'Ensepulcher' then 
+			elseif res.monster_abilities[act.targets[1].actions[1].param].en == 'Ensepulcher' then 
 				chat('/'..chatmode..' Aero (Ensepulcher) <call14>')
-			elseif res.monster_abilities[targets[1].actions[1].param].en == 'Ceaseless Surge' then 
+			elseif res.monster_abilities[act.targets[1].actions[1].param].en == 'Ceaseless Surge' then 
 				chat('/'..chatmode..' Stone (Ceaseless Surge) <call14>')
-			elseif res.monster_abilities[targets[1].actions[1].param].en == 'Blast of Reticence' then 
+			elseif res.monster_abilities[act.targets[1].actions[1].param].en == 'Blast of Reticence' then 
 				chat('/'..chatmode..' Blizzard (Blast of Reticence) <call14>')
 			end
 
 		elseif actor.name == "Glassy Thinker" then
-			if res.monster_abilities[targets[1].actions[1].param].en == 'Pain Sync' then 
+			if res.monster_abilities[act.targets[1].actions[1].param].en == 'Pain Sync' then 
 				chat('/'..chatmode..' PAIN SYNC <call14>')
 			end
 
 		elseif actor.name == "OU" or actor.name == "Kin" then
-			if res.monster_abilities[targets[1].actions[1].param].en == 'Target' then
+			if res.monster_abilities[act.targets[1].actions[1].param].en == 'Target' then
 				chat('/'..chatmode..' Target used on '..target_name..' <call14>')
+			end
+
+		elseif actor.name == "Bumba" then
+			if res.monster_abilities[act.targets[1].actions[1].param].en == 'Perfect Dodge' then
+				chat('/'..chatmode..' Perfect Dodge <call14>')
+				chat:schedule(30,'/'..chatmode..' Perfect Dodge is off <call14>')
+			elseif res.monster_abilities[act.targets[1].actions[1].param].en == 'Invincible' then
+				chat('/'..chatmode..' Invincible <call14>')
+				chat:schedule(30,'/'..chatmode..' Invincible is off <call14>')
+			elseif res.monster_abilities[act.targets[1].actions[1].param].en == 'Elemental Sforzo' then
+				chat('/'..chatmode..' Elemental Sforzo <call14>')
+				chat:schedule(30,'/'..chatmode..' Elemental Sforzo is off <call14>')
+			elseif res.monster_abilities[act.targets[1].actions[1].param].en == 'Yaegasumi' then
+				chat('/'..chatmode..' Yaegasumi <call14>')
+				chat:schedule(45,'/'..chatmode..' Yaegasumi is off <call14>')
 			end
 		end
 
@@ -99,36 +114,36 @@ windower.register_event('incoming text',function(org)
 
 	if org:find('You find a') then
 		if org:find('Fu\'s scale') then
-			command('wait 2 5;input /'..chatmode..' Fu\'s Scale: BST, DRG, SMN, PUP <call14>')
+			cmd('wait 2 5;input /'..chatmode..' Fu\'s Scale: BST, DRG, SMN, PUP <call14>')
 		elseif org:find('Gin\'s scale') then
-			command('wait 2 5;input /'..chatmode..' Gin\'s Scale: THF, NIN, DNC, RUN <call14>')
+			cmd('wait 2 5;input /'..chatmode..' Gin\'s Scale: THF, NIN, DNC, RUN <call14>')
 		elseif org:find('Kei\'s scale') then
-			command('wait 2 5;input /'..chatmode..' Kei\'s Scale: WHM, BLM, RDM, BLU, SCH <call14>')
+			cmd('wait 2 5;input /'..chatmode..' Kei\'s Scale: WHM, BLM, RDM, BLU, SCH <call14>')
 		elseif org:find('Kin\'s scale') then
-			command('wait 2 5;input /'..chatmode..' Kin\'s Scale: WAR, MNK, PLD, DRK, SAM <call14>')
+			cmd('wait 2 5;input /'..chatmode..' Kin\'s Scale: WAR, MNK, PLD, DRK, SAM <call14>')
 		elseif org:find('Kyou\'s scale') then
-			command('wait 2 5;input /'..chatmode..' Kyou\'s Scale: BRD, RNG, COR, GEO <call14>')
+			cmd('wait 2 5;input /'..chatmode..' Kyou\'s Scale: BRD, RNG, COR, GEO <call14>')
 		end
 
 	elseif org:find('You pitiful lot will never learn') then
-		command('input /'..chatmode..' Perfidien pop! <call14>')
+		cmd('input /'..chatmode..' Perfidien pop! <call14>')
 
 	elseif org:find('the void calls') then
-		command('input /'..chatmode..' Plouton pop! <call14>')
+		cmd('input /'..chatmode..' Plouton pop! <call14>')
 
 	elseif org:find('Hoho! Poked at a sore spot, didn\'t you?') or org:find('Switching things up, hmm?') then
 		if org:find('Lightning') then
-			command('input /'..chatmode..' Thunder <call14>')
+			cmd('input /'..chatmode..' Thunder <call14>')
 		elseif org:find('Fire') then
-			command('input /'..chatmode..' Fire <call14>')
+			cmd('input /'..chatmode..' Fire <call14>')
 		elseif org:find('Wind') then
-			command('input /'..chatmode..' Aero <call14>')
+			cmd('input /'..chatmode..' Aero <call14>')
 		elseif org:find('Earth') then
-			command('input /'..chatmode..' Stone <call14>')
+			cmd('input /'..chatmode..' Stone <call14>')
 		elseif org:find('Ice') then
-			command('input /'..chatmode..' Blizzard <call14>')
+			cmd('input /'..chatmode..' Blizzard <call14>')
 		elseif org:find('Water') then
-			command('input /'..chatmode..' Water <call14>')
+			cmd('input /'..chatmode..' Water <call14>')
 		end
 
 	end
@@ -138,7 +153,8 @@ end)
 function display_help()
 	windower.add_to_chat(200,'[Callouts] '..('Use \'//co chatmode\' to switch between Echo and Party chat modes'):color(8)..'')
 	windower.add_to_chat(200,'[Callouts] '..('Current callouts:'):color(8)..'')
-	windower.add_to_chat(200,'[Callouts] '..('Omen (Scales, Pain Sync)'):color(8)..'')
+	windower.add_to_chat(200,'[Callouts] '..('Odyssey (Bumba 1-HRs)'):color(8)..'')
+	windower.add_to_chat(200,'[Callouts] '..('Omen (Scales, Pain Sync, Target)'):color(8)..'')
 	windower.add_to_chat(200,'[Callouts] '..('Vagary (Perfidien/Plouton pop, weaknesses'):color(8)..'')
 	windower.add_to_chat(200,'[Callouts] '..('Sortie (weaknesses)'):color(8)..'')
 end
@@ -156,6 +172,10 @@ windower.register_event('addon command', function(addcmd)
 			chatmode = 'echo'
 			windower.add_to_chat(200,'[Callouts] '..('Chat mode is now set to Echo'):color(8)..'')
 		end
+
+	elseif addcmd == 'reload' then
+        cmd('lua r callouts')
+        return
 
 	else
 		windower.add_to_chat(200,'[Callouts] '..('Unknown command'):color(8)..'')
