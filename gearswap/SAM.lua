2456ab830eb1d87f7f4f872396aeda4fef5b148e
iLVL-Key <101156258+iLVL-Key@users.noreply.github.com>
2023-02-07 13:15:31 -0600

    02.07.23 (VCC: Tachi: Yukikaze)
    -Adjusted WS Damage Notification to filter out some Job Abilities that get listed in the same action category as Weapon Skills.

--- gearswap/SAM.lua
+++ gearswap/SAM.lua
@@ -70,8 +70,8 @@ DoomOnText		=	'doom'			--		Text that displays in party chat when you are doomed.
 DoomOffText		=	'doom off'		--		That that displays in party chat when you are no longer doomed.
 
 -- Heads Up Display --
-HUDposX			=	967		--				X position for the HUD. 0 is left of the window, increasing this number will move it to the right.
-HUDposYLine1	=	745		--				Y position for the HUD. 0 is top of the window, increasing this number will move it downward.
+HUDposX			=	965		--				X position for the HUD. 0 is left of the window, increasing this number will move it to the right.
+HUDposYLine1	=	794		--				Y position for the HUD. 0 is top of the window, increasing this number will move it downward.
 							--				Note that this is for the first line of the HUD, the other lines will self-adjust. If you cannot
 							--				see the HUD, set the x and y both to 100 to make sure it is showing up, then adjust from there.
 FontSize		=	12		--				Font size. Changing this will require you to adjust the Spacers below as well.
@@ -268,7 +268,7 @@ function get_sets()
 		head="Mpaca's Cap",
 		body="Sakonji Domaru +3",
 		hands="Kasuga Kote +3",
-		legs="Wakido Haidate +3",
+		legs="Nyame Flanchard",
 		feet="Nyame Sollerets",
 		neck="Sam. Nodowa +2",
 		waist="Sailfi Belt +1",
@@ -286,10 +286,10 @@ function get_sets()
 		head="Mpaca's Cap",
 		body="Sakonji Domaru +3",
 		hands="Kasuga Kote +3",
-		legs="Wakido Haidate +3",
+		legs="Nyame Flanchard",
 		feet="Nyame Sollerets",
 		neck="Sam. Nodowa +2",
-		waist="Sailfi Belt +1",
+		waist="Fotia Belt",
 		left_ear="Moonshade Earring",
 		right_ear="Thrud Earring",
 		left_ring="Karieyh Ring +1",
@@ -450,7 +450,7 @@ TopVersion = 'Tachi: Yukikaze' --Leave this alone, used for debugging purposes
 
 
 BottomVersion = 'Tachi: Yukikaze'
-FileVersion = '01.24.23'
+FileVersion = '02.07.23'
 
 -------------------------------------------
 --               UPDATES                 --
@@ -460,6 +460,9 @@ FileVersion = '01.24.23'
 If the new updates Version Compatibility Codename matches your current files TopVersion,
 simply replace everything under the "Do Not Edit Below This Line".
 Only when the Version Compatibility Codename changes will you need to update the entire file.
+
+02.07.23 (Version Compatibility Codename: Tachi: Yukikaze)
+-Adjusted WS Damage Notification to filter out some Job Abilities that get listed in the same action category as Weapon Skills.
 
 01.24.23 (Version Compatibility Codename: Tachi: Yukikaze)
 -Adjusted WS Damage Notification to display Skillchain damage.
@@ -2454,9 +2457,9 @@ windower.register_event('action',function(act)
 		--Uses Weapon Skill but misses, gets blinked, or hits for 0
 		if act.targets[1].actions[1].message == 188 or act.targets[1].actions[1].message == 31 or (act.targets[1].actions[1].message == 185 and act.targets[1].actions[1].param == 0) then
 			send_command('wait .2;text notifications text "«« '..weaponskills[act.param].english..' Missed »»";text notifications color 0 255 255;text notifications bg_transparency 1')
-		elseif act.targets[1].actions[1].has_add_effect == true then
+		elseif act.targets[1].actions[1].message == 185 and act.targets[1].actions[1].has_add_effect == true then
 			send_command('wait .2;text notifications text "'..weaponskills[act.param].english..': '..act.targets[1].actions[1].param..' ('..sc[act.targets[1].actions[1].add_effect_animation]..': '..act.targets[1].actions[1].add_effect_param..')";text notifications color 0 255 255;text notifications bg_transparency 1')
-		else
+		elseif act.targets[1].actions[1].message == 185 then
 			send_command('wait .2;text notifications text "'..weaponskills[act.param].english..': '..act.targets[1].actions[1].param..'";text notifications color 0 255 255;text notifications bg_transparency 1')
 		end
 		NotiCountdown = -1
