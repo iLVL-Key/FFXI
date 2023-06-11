05a6a7bf656a818dd464466de431edc892dac873
Key <101156258+iLVL-Key@users.noreply.github.com>
2023-06-10 23:39:26 -0500

    Update to 2.0
    Major update with loads of additions and refinements

--- addons/battleplan/battleplan.lua
+++ addons/battleplan/battleplan.lua
@@ -27,22 +27,22 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ]]
 
 _addon.name = 'Battle Plan'
-_addon.version = '1.0.0'
+_addon.version = '2.0.0'
 _addon.author = 'Key'
 _addon.commands = {'battleplan','bp'}
 
 require 'logger'
+texts = require('texts')
 config = require('config')
 
 tb_name = 'addon:battleplan'
+visible = true
 
 defaults = T{}
-defaults.light     = false
-defaults.timer     = false
 
 defaults.position = T{}
 defaults.position.x = 0
-defaults.position.y = 350
+defaults.position.y = (windower.get_windower_settings().ui_y_res) / 2 --Makes the default Y position in the middle of your screen
 
 defaults.font = T{}
 defaults.font.family = 'Arial'
@@ -57,20 +57,27 @@ defaults.colors.background.r = 0
 defaults.colors.background.g = 43
 defaults.colors.background.b = 54
 defaults.colors.background.a = 200
-defaults.colors.title = T{}
-defaults.colors.title.r = 220
-defaults.colors.title.g = 50
-defaults.colors.title.b = 47
-defaults.colors.value = T{}
-defaults.colors.value.r = 147
-defaults.colors.value.g = 161
-defaults.colors.value.b = 161
+defaults.colors.header = T{}
+defaults.colors.header.r = 220
+defaults.colors.header.g = 50
+defaults.colors.header.b = 47
+defaults.colors.text = T{}
+defaults.colors.text.r = 147
+defaults.colors.text.g = 161
+defaults.colors.text.b = 161
 
 settings = config.load(defaults)
 
-line1 = ""
-line2 = ""
-line3 = ""
+local chat = windower.chat.input
+local cmd = windower.send_command
+
+line = {}
+line[1] = ""
+line[2] = ""
+line[3] = ""
+line[4] = ""
+line[5] = ""
+
 
 function initialize()
     local background = settings.colors.background
@@ -91,79 +98,371 @@ end
 windower.register_event('load', initialize)
 
 function updateBox()
-    local text = T{
-        ' \\cs('..settings.colors.title.r..', '..settings.colors.title.g..', '..settings.colors.title.b..')--== Battle Plan ==--\\cr \n',
-        ' \\cs('..settings.colors.value.r..', '..settings.colors.value.g..', '..settings.colors.value.b..')'..line1..'\\cr \n',
-        ' \\cs('..settings.colors.value.r..', '..settings.colors.value.g..', '..settings.colors.value.b..')'..line2..'\\cr \n',
-        ' \\cs('..settings.colors.value.r..', '..settings.colors.value.g..', '..settings.colors.value.b..')'..line3..'\\cr \n',
-    }
-    windower.text.set_text(tb_name, text:concat(''))
+
+    if visible then
+        windower.text.set_visibility(tb_name, true)
+    end
+
+    local textColor = settings.colors.text
+    local headerColor = settings.colors.header
+
+    -- Create a temporary "text" array that we use to build what's displayed in the box
+    local text = T{}
+    text[1] = ' \\cs('..headerColor.r..', '..headerColor.g..', '..headerColor.b..')--== Battle Plan ==--\\cr \n'
+
+    -- If Line 5 is not empty, we display it and every line above it plus the BP header
+    if line[5] ~= "" then
+        text[2] = ' \\cs('..textColor.r..', '..textColor.g..', '..textColor.b..')'..line[1]..'\\cr \n'
+        text[3] = ' \\cs('..textColor.r..', '..textColor.g..', '..textColor.b..')'..line[2]..'\\cr \n'
+        text[4] = ' \\cs('..textColor.r..', '..textColor.g..', '..textColor.b..')'..line[3]..'\\cr \n'
+        text[5] = ' \\cs('..textColor.r..', '..textColor.g..', '..textColor.b..')'..line[4]..'\\cr \n'
+        text[6] = ' \\cs('..textColor.r..', '..textColor.g..', '..textColor.b..')'..line[5]..'\\cr \n'
+    
+    -- If Line 5 is empty, but Line 4 is not empty, we display it and every line above it plus the BP header
+    elseif line[4] ~= "" then
+        text[2] = ' \\cs('..textColor.r..', '..textColor.g..', '..textColor.b..')'..line[1]..'\\cr \n'
+        text[3] = ' \\cs('..textColor.r..', '..textColor.g..', '..textColor.b..')'..line[2]..'\\cr \n'
+        text[4] = ' \\cs('..textColor.r..', '..textColor.g..', '..textColor.b..')'..line[3]..'\\cr \n'
+        text[5] = ' \\cs('..textColor.r..', '..textColor.g..', '..textColor.b..')'..line[4]..'\\cr \n'
+    
+    -- If Lines 4-5 are empty, but Line 3 is not empty, we display it and every line above it plus the BP header
+    elseif line[3] ~= "" then
+        text[2] = ' \\cs('..textColor.r..', '..textColor.g..', '..textColor.b..')'..line[1]..'\\cr \n'
+        text[3] = ' \\cs('..textColor.r..', '..textColor.g..', '..textColor.b..')'..line[2]..'\\cr \n'
+        text[4] = ' \\cs('..textColor.r..', '..textColor.g..', '..textColor.b..')'..line[3]..'\\cr \n'
+    
+    -- If Lines 3-5 are empty, but Line 2 is not empty, we display it and every line above it plus the BP header
+    elseif line[2] ~= "" then
+        text[2] = ' \\cs('..textColor.r..', '..textColor.g..', '..textColor.b..')'..line[1]..'\\cr \n'
+        text[3] = ' \\cs('..textColor.r..', '..textColor.g..', '..textColor.b..')'..line[2]..'\\cr \n'
+    
+    -- If Lines 2-5 are empty, but Line 1 is not empty, we display it plus the BP header
+    elseif line[1] ~= "" then
+        text[2] = ' \\cs('..textColor.r..', '..textColor.g..', '..textColor.b..')'..line[1]..'\\cr \n'
+    
+    -- If all lines are empty, we hide the whole box
+    else
+        hideBox()
+    end
+
+    -- Now we take our temporary "text" array and turn it into a string and update the text inside the box
+    windower.text.set_text(tb_name, text:concat(''))    
+
 end
 
+-- Update Line 1
 function getLine1(input)
+
     -- Take the entire line and put it into a string
     local inputString = input
 
-    -- Extract the text after "!bp1" but before "!bp2" or "!bp3" (if they exist)
-    local extractedText = string.match(inputString, "!bp1%s*(.-)%s*!bp2") or string.match(inputString, "!bp1%s*(.-)%s*!bp3") or string.match(inputString, "!bp1%s*(.-)$")
+    -- Remove the text before and including "!bp1"
+    local extractedText = string.match(inputString, "!bp1%s*(.-)$")
     
-    -- Refine the text to catch any leftovers
-    local refinedText = string.match(extractedText, "^(.-)!bp3") or extractedText
+    -- Remove any text after and including "!bp2", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp2") or extractedText
 
-    -- Save the refined text to a variable so we can use it later
-    line1 = refinedText
+    -- Remove any text after and including "!bp3", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp3") or extractedText
 
-    -- Now that we have our desired text extracted we update the on-screen box with it
-    updateBox()
+    -- Remove any text after and including "!bp4", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp4") or extractedText
+
+    -- Remove any text after and including "!bp5", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp5") or extractedText
+
+    -- Return the text we extracted to what called for it
+    return extractedText
+
 end
 
+-- Update Line 2
 function getLine2(input)
+
     -- Take the entire line and put it into a string
     local inputString = input
 
-    -- Extract the text after "!bp2" but before "!bp1" or "!bp3" (if they exist)
-    local extractedText = string.match(inputString, "!bp2%s*(.-)%s*!bp1") or string.match(inputString, "!bp2%s*(.-)%s*!bp3") or string.match(inputString, "!bp2%s*(.-)$")
+    -- Remove the text before and including "!bp2"
+    local extractedText = string.match(inputString, "!bp2%s*(.-)$")
     
-    -- Refine the text to catch any leftovers
-    local refinedText = string.match(extractedText, "^(.-)!bp3") or extractedText
+    -- Remove any text after and including "!bp1", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp1") or extractedText
 
-    -- Save the refined text to a variable so we can use it later
-    line2 = refinedText
+    -- Remove any text after and including "!bp3", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp3") or extractedText
 
-    -- Now that we have our desired text extracted we update the on-screen box with it
-    updateBox()
+    -- Remove any text after and including "!bp4", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp4") or extractedText
+
+    -- Remove any text after and including "!bp5", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp5") or extractedText
+
+    -- Return the text we extracted to what called for it
+    return extractedText
+
 end
 
+-- Update Line 3
 function getLine3(input)
+
     -- Take the entire line and put it into a string
     local inputString = input
 
-    -- Extract the text after "!bp3" but before "!bp1" or "!bp2" (if they exist)
-    local extractedText = string.match(inputString, "!bp3%s*(.-)%s*!bp1") or string.match(inputString, "!bp3%s*(.-)%s*!bp2") or string.match(inputString, "!bp3%s*(.-)$")
+    -- Remove the text before and including "!bp3"
+    local extractedText = string.match(inputString, "!bp3%s*(.-)$")
     
-    -- Refine the text to catch any leftovers
-    local refinedText = string.match(extractedText, "^(.-)!bp2") or extractedText
+    -- Remove any text after and including "!bp1", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp1") or extractedText
+
+    -- Remove any text after and including "!bp2", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp2") or extractedText
+
+    -- Remove any text after and including "!bp4", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp4") or extractedText
+
+    -- Remove any text after and including "!bp5", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp5") or extractedText
+
+    -- Return the text we extracted to what called for it
+    return extractedText
+
+end
+
+-- Update Line 4
+function getLine4(input)
+
+    -- Take the entire line and put it into a string
+    local inputString = input
+
+    -- Remove the text before and including "!bp4"
+    local extractedText = string.match(inputString, "!bp4%s*(.-)$")
+    
+    -- Remove any text after and including "!bp1", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp1") or extractedText
+
+    -- Remove any text after and including "!bp2", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp2") or extractedText
+
+    -- Remove any text after and including "!bp3", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp3") or extractedText
+
+    -- Remove any text after and including "!bp5", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp5") or extractedText
 
     -- Save the refined text to a variable so we can use it later
-    line3 = refinedText
+    line[4] = extractedText
 
-    -- Now that we have our desired text extracted we update the on-screen box with it
+    -- Return the text we extracted to what called for it
+    return extractedText
+
+end
+
+-- Update Line 5
+function getLine5(input)
+
+    -- Take the entire line and put it into a string
+    local inputString = input
+
+    -- Remove the text before and including "!bp5"
+    local extractedText = string.match(inputString, "!bp5%s*(.-)$")
+    
+    -- Remove any text after and including "!bp1", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp1") or extractedText
+
+    -- Remove any text after and including "!bp2", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp2") or extractedText
+
+    -- Remove any text after and including "!bp3", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp3") or extractedText
+
+    -- Remove any text after and including "!bp4", if found
+    local extractedText = string.match(extractedText, "^(.-)!bp4") or extractedText
+
+    -- Return the text we extracted to what called for it
+    return extractedText
+
+end
+
+-- Show the BP box
+function showBox()
+    windower.text.set_visibility(tb_name, true)
     updateBox()
 end
 
-windower.register_event('incoming text',function(command, ...)
+-- Hide the BP box
+function hideBox()
+    windower.text.set_visibility(tb_name, false)
+end
 
-    -- Look for the signals for Battle Plan to do its thing
-    if command:find('!bp1') then
-        getLine1(...)
+-- Toggle the visibility of the BP box
+function toggleBox()
+    if visible then
+        visible = false
+        hideBox()
+    else
+        visible = true
+        showBox()
+    end
+end
+
+
+-- Clear the appropriate BP box
+function clear(who)
+    if who == "mine" then
+        line[1] = ""
+        line[2] = ""
+        line[3] = ""
+        line[4] = ""
+        line[5] = ""
+        updateBox()
+        windower.add_to_chat(200,'[Battle Plan] '..('Clearing YOUR BP box. Other people\'s BP boxes remain the same.'):color(8)..'')
+    elseif who == "everyone" then
+        chat('/p !bpclear')
+        windower.add_to_chat(200,'[Battle Plan] '..('Clearing EVERYONE\'S BP box.'):color(8)..'')
+    end
+end
+
+function displayHelp()
+    windower.add_to_chat(200,'[Battle Plan] '..('A collaborative "whiteboard" to keep you and your party members on the same page.'):color(8)..'')
+    windower.add_to_chat(200,'[Battle Plan] '..('Up to 5 lines are available to use. BP box is not visible when all lines are empty.'):color(8)..'')
+    windower.add_to_chat(200,'[Battle Plan] '..('Commands:'):color(8)..'')
+	windower.add_to_chat(200,'[Battle Plan] '..(' show - Show the BP box.'):color(8)..'')
+    windower.add_to_chat(200,'[Battle Plan] '..(' hide - Hide the BP box.'):color(8)..'')
+    windower.add_to_chat(200,'[Battle Plan] '..(' visible - Toggle the BP box visibility (alternative to show/hide).'):color(8)..'')
+    windower.add_to_chat(200,'[Battle Plan] '..(' clear - Clear EVERYONE\'S BP box.'):color(8)..'')
+    windower.add_to_chat(200,'[Battle Plan] '..(' clearmine - Clear YOUR BP box. Other people\'s BP boxes remain the same.'):color(8)..'')
+    windower.add_to_chat(200,'[Battle Plan] '..(' line#/l#/# - Create party text to update the BP box for all members running BP.'):color(8)..'')
+    windower.add_to_chat(200,'[Battle Plan] '..('               # must be a number 1-5. (Ex. //bp 3 Hello from iLVL of Valefor!)'):color(8)..'')
+    windower.add_to_chat(200,'[Battle Plan] '..(' pos x y - Update the position of the BP box. (Current: '..settings.position.x..' '..settings.position.y..', Default: '..defaults.position.x..' '..defaults.position.y..')'):color(8)..'')
+    windower.add_to_chat(200,'[Battle Plan] '..(' size # - Update the font size of the BP box. (Current: '..settings.font.size..', Default: '..defaults.font.size..')'):color(8)..'')
+end
+
+-- "Party Commands" issued through party chat, denoted by starting with !bp
+windower.register_event('incoming text',function(partycmd, ...)
+
+    -- Update Line 1
+    if partycmd:find('!bp1') then
+        line[1] = getLine1(...)
+        updateBox()
     end
 
-    if command:find('!bp2') then
-        getLine2(...)
+    -- Update Line 2
+    if partycmd:find('!bp2') then
+        line[2] = getLine2(...)
+        updateBox()
     end
 
-    if command:find('!bp3') then
-        getLine3(...)
+    -- Update Line 3
+    if partycmd:find('!bp3') then
+        line[3] = getLine3(...)
+        updateBox()
     end
 
+    -- Update Line 4
+    if partycmd:find('!bp4') then
+        line[4] = getLine4(...)
+        updateBox()
+    end
+
+    -- Update Line 5
+    if partycmd:find('!bp5') then
+        line[5] = getLine5(...)
+        updateBox()
+    end
+
+    if partycmd:find('!bpclear') then
+        clear('mine')
+    end
+
+end)
+
+-- "Addon Commands" issued through the chat log, denoted by starting with //battleplan or //bp
+windower.register_event('addon command',function(addcmd, ...)
+
+    -- Show the BP box
+    if addcmd == 'show' then
+        visible = true
+        showBox()
+    
+    -- Hide the BP box
+    elseif addcmd == 'hide' then
+        visible = false
+        hideBox()
+
+    -- Toggle the visibility of the BP box
+    elseif addcmd == 'visible' then
+        toggleBox()
+
+    -- Clear EVERYONE'S BP box (or only yours if you add 'mine' after)
+    elseif addcmd == 'clear' then
+        if ... == "mine" then
+            clear('mine')
+        else
+            clear('everyone')
+        end
+
+    -- Clear YOUR BP box
+    elseif addcmd == 'clearmine' then
+        clear('mine')
+
+    -- Update Line 1
+    elseif addcmd == 'line1' or addcmd == 'l1' or addcmd == '1' then
+        local line = table.concat({...}, " ")
+        chat('!bp1 ' ..line)
+
+    -- Update Line 2
+    elseif addcmd == 'line2' or addcmd == 'l2' or addcmd == '2' then
+        local line = table.concat({...}, " ")
+        chat('!bp2 ' ..line)
+
+    -- Update Line 3
+    elseif addcmd == 'line3' or addcmd == 'l3' or addcmd == '3' then
+        local line = table.concat({...}, " ")
+        chat('!bp3 ' ..line)
+
+    -- Update Line 4
+    elseif addcmd == 'line4' or addcmd == 'l4' or addcmd == '4' then
+        local line = table.concat({...}, " ")
+        chat('!bp4 ' ..line)
+
+    -- Update Line 5
+    elseif addcmd == 'line5' or addcmd == 'l5' or addcmd == '5' then
+        local line = table.concat({...}, " ")
+        chat('!bp5 ' ..line)
+
+    elseif addcmd == 'pos' or addcmd == 'position' then
+        local pos = {...}
+        if #pos < 2 then
+            windower.add_to_chat(200,'[Battle Plan] '..('Error: Need both X and Y coordinates (ex. //bp pos 100 200). Current position: '..settings.position.x..' '..settings.position.y):color(8)..'')
+            return
+        end
+        settings.position.x = tonumber(pos[1])
+        settings.position.y = tonumber(pos[2])
+        settings:save('all')
+        windower.text.set_location(tb_name, settings.position.x, settings.position.y)
+        windower.add_to_chat(200,'[Battle Plan] '..('Position updated: '..settings.position.x..' '..settings.position.y):color(8)..'')
+
+    elseif addcmd == 'size' or addcmd == 'fontsize' then
+        local size = {...}
+        settings.font.size = tonumber(size[1])
+        settings:save('all')
+        windower.text.set_font_size(tb_name, settings.font.size)
+        windower.add_to_chat(200,'[Battle Plan] '..('Font Size updated: '..settings.font.size):color(8)..'')
+
+    -- Reload Battle Plan
+    elseif addcmd == 'reload' then
+        cmd('lua r battleplan')
+
+    -- Unload Battle Plan
+    elseif addcmd == 'unload' then
+        windower.add_to_chat(200,'[Battle Plan] '..('Unloaded. Type \'//lua l battleplan\' if you wish to load again.'):color(8)..'')
+        cmd('lua u battleplan')
+    
+    elseif addcmd == 'help' then
+        displayHelp()
+
+    else
+        windower.add_to_chat(200,'[Battle Plan] '..('Unrecognized command. Type \'//bp help\' if you need help.'):color(8)..'')
+
+    end
 end)
