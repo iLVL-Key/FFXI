5f966515abbf23855c18d1d71c77bfdd586491c6
Key <101156258+iLVL-Key@users.noreply.github.com>
2024-05-03 21:03:30 -0500

    Version 1.8

--- addons/callouts/callouts.lua
+++ addons/callouts/callouts.lua
@@ -25,7 +25,7 @@
 --SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 _addon.name = 'Callouts'
-_addon.version = '1.6'
+_addon.version = '1.8'
 _addon.author = 'Key (Keylesta@Valefor)'
 _addon.commands = {'callouts','co'}
 
@@ -39,6 +39,31 @@ defaults.chatmode = 'echo'
 settings = config.load(defaults)
 
 local chat = windower.chat.input
+local Heartbeat = 0
+
+local Ou_pop = false
+local Ou_100 = false
+local Ou_95 = false
+local Ou_75 = false
+local Ou_65 = false
+local Ou_60 = false
+local Ou_55 = false
+local Ou_45 = false
+local Ou_30 = false
+local Ou_15 = false
+local Ou_10 = false
+local Ou_Ebullient_used = false
+local Ou_Bravado_used = false
+local Ou_Fullers_used = false
+local Ou_Zero_Hour_used = false
+local Ou_Target_used = false
+local Ou_Gardez_used = false
+
+local charmed = T{}
+
+local ally_pos = {
+	'p0', 'p1', 'p2', 'p3', 'p4', 'p5', 'a10', 'a11', 'a12', 'a13', 'a14', 'a15', 'a20', 'a21', 'a22', 'a23', 'a24', 'a25'
+}
 
 windower.register_event('action',function(act)
 
@@ -50,21 +75,28 @@ windower.register_event('action',function(act)
 	local monster_ability = res.monster_abilities[act.targets[1].actions[1].param]
 	local spell_start = res.spells[act.targets[1].actions[1].param]
 
-	-- A player is charmed
-	if act.category == 11 and (act.targets[1].actions[1].param == 14 or act.targets[1].actions[1].param == 17) and act.targets[1].actions[1].message == 242 then
-		chat(('/%s \r \r'..windower.ffxi.get_mob_by_id(act.targets[1].id).name..' is charmed!%s\r \r'):format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+	if act.category == 11 then
+		if actor == 'Bumba' then
+			local monster_ability = res.monster_abilities[act.param]
+			if monster_ability == nil then
+				return
+			elseif monster_ability.en == 'Perfect Dodge' then
+				chat('/%s Perfect Dodge%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+				chat:schedule(30,'/%s Perfect Dodge is off%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+			elseif monster_ability.en == 'Invincible' then
+				chat('/%s Invincible%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+				chat:schedule(30,'/%s Invincible is off%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+			elseif monster_ability.en == 'Elemental Sforzo' then
+				chat('/%s Elemental Sforzo%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+				chat:schedule(30,'/%s Elemental Sforzo is off%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+			elseif monster_ability.en == 'Yaegasumi' then
+				chat('/%s Yaegasumi%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+				chat:schedule(45,'/%s Yaegasumi is off%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+			elseif monster_ability.en == 'Brazen Rush' then
+				chat('/%s Brazen Rush%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+			end
+		end
 	end
-
-
-
-
-	-- if actor == 'Bumba' or actor == 'Bozzetto Autarch' then
-	if actor == 'Bumba' then
-		chat('/echo (Actor: '..actor..') (Cat: '..act.category..') (Param: '..act.param..') (Msg: '..act.targets[1].actions[1].message..') (target.actions.param: '..act.targets[1].actions[1].param..') (Target: '..windower.ffxi.get_mob_by_id(act.targets[1].id).name..')')
-	end
-
-
-
 
 	if act.category == 8 then
 		if actor == 'Bozzetto Autarch' then
@@ -87,22 +119,6 @@ windower.register_event('action',function(act)
 	end
 
 	if act.category == 7 then -- initiation of weapon skill or monster TP move
-
-		-- if actor == 'Locus Camelopard' then
-			-- if monster_ability == nil then
-				-- return
-			-- else
-				-- chat('/%s %s used %s on %s%s':format(settings.chatmode,actor,monster_ability.en,target_name,settings.chatmode == 'party' and ' <call14>' or ''))
-			-- end
-		-- end
-
-		-- if actor == 'Locus Dire Bat' then
-		-- 	if monster_ability == nil then
-		-- 		return
-		-- 	else
-		-- 		chat('/%s %s used %s%s':format(settings.chatmode,actor,monster_ability.en,settings.chatmode == 'party' and ' <call14>' or ''))
-		-- 	end
-		-- end
 
 		if actor == 'Aita' or actor == 'Degei' then
 			if monster_ability == nil then
@@ -169,13 +185,33 @@ windower.register_event('action',function(act)
 		elseif actor == 'Ou' or actor == 'Kin' then
 			if monster_ability == nil then
 				return
+			elseif monster_ability.en == 'Ebullient Nullification' then
+				Ou_Ebullient_used = true
+			elseif monster_ability.en == 'Unfaltering Bravado' then
+				Ou_Bravado_used = true
+			elseif monster_ability.en == 'Dancing Fullers' then
+				Ou_Fullers_used = true
+			elseif monster_ability.en == 'Zero Hour' then
+				Ou_Zero_Hour_used = true
 			elseif monster_ability.en == 'Target' then
 				chat('/%s Target used on %s%s':format(settings.chatmode,windower.ffxi.get_mob_by_id(act.targets[1].id).name,settings.chatmode == 'party' and ' <call14>' or ''))
+				Ou_Target_used = true
+			elseif monster_ability.en == 'Gardez' then
+				Ou_Gardez_used = true
 			elseif monster_ability.en == 'Prophylaxis' then
 				chat('/%s Prophylaxis - 30 second countdown to reset%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
-				windower.send_command('wait 10;input /%s 20 seconds to reset%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
-				windower.send_command('wait 20;input /%s 10 seconds to reset%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
-				windower.send_command('wait 25;input /%s 5 seconds to reset%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+				coroutine.sleep(10)
+				if windower.ffxi.get_mob_by_name('Ou').hpp ~= 0 then
+					chat('/%s 20 seconds to reset%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+					coroutine.sleep(10)
+					if windower.ffxi.get_mob_by_name('Ou').hpp ~= 0 then
+						chat('/%s 10 seconds to reset%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+						coroutine.sleep(5)
+						if windower.ffxi.get_mob_by_name('Ou').hpp ~= 0 then
+							chat('/%s 5 seconds to reset%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+						end
+					end
+				end
 			end
 
 		elseif actor == 'Arebati' then
@@ -183,23 +219,6 @@ windower.register_event('action',function(act)
 				return
 			elseif monster_ability.en == 'Glassy Nova' then
 				chat('/%s Full Dispel%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
-			end
-
-		elseif actor == 'Bumba' then
-			if monster_ability == nil then
-				return
-			elseif monster_ability.en == 'Perfect Dodge' then
-				chat('/%s Perfect Dodge%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
-				chat:schedule(30,'/%s Perfect Dodge is off%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
-			elseif monster_ability.en == 'Invincible' then
-				chat('/%s Invincible%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
-				chat:schedule(30,'/%s Invincible is off%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
-			elseif monster_ability.en == 'Elemental Sforzo' then
-				chat('/%s Elemental Sforzo%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
-				chat:schedule(30,'/%s Elemental Sforzo is off%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
-			elseif monster_ability.en == 'Yaegasumi' then
-				chat('/%s Yaegasumi%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
-				chat:schedule(30,'/%s Yaegasumi is off%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
 			end
 
 		elseif actor == 'Gigelorum' then
@@ -246,6 +265,10 @@ windower.register_event('action',function(act)
 				return
 			elseif monster_ability.en == 'Incisive Denouement' then
 				chat('/%s Hate Reset%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+			elseif monster_ability.en == 'Stinger Volley' then
+				chat('/%s Zombie%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+			elseif monster_ability.en == 'Droning Whirlwind' then
+				chat('/%s Fetters incoming!%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
 			end
 
 		elseif actor == 'Halphas' then
@@ -306,6 +329,86 @@ windower.register_event('incoming text',function(org)
 	end
 end)
 
+function resetOu()
+	Ou_pop = false
+	Ou_100 = false
+	Ou_95 = false
+	Ou_75 = false
+	Ou_65 = false
+	Ou_60 = false
+	Ou_55 = false
+	Ou_45 = false
+	Ou_30 = false
+	Ou_15 = false
+	Ou_10 = false
+	Ou_Ebullient_used = false
+	Ou_Bravado_used = false
+	Ou_Fullers_used = false
+	Ou_Zero_Hour_used = false
+	Ou_Target_used = false
+	Ou_Gardez_used = false
+end
+
+function OuCallouts()
+	local Ou = windower.ffxi.get_mob_by_name('Ou')
+	local Ou_nearby = Ou and Ou.valid_target
+
+	if Ou_nearby then
+		if not Ou_pop then
+			Ou_pop = true
+			chat('/%s 95%%:Ebullient 75%%:Bravado 60%%:Fullers 45%%:Zero Hour 30%%:Target 15%%:Gardez 10%%:Prophalaxis':format(settings.chatmode))
+		elseif not Ou_100 and Ou.claim_id ~= 0 then --Claimed, start of fight
+			Ou_100 = true
+			chat('/%s First up @ 95%% - Ebullient Nullification: Absorbs all buffs within 20\' AoE (sweet spot: 5~8 buffs)%s':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+		elseif not Ou_95 and Ou_Ebullient_used then
+			Ou_95 = true
+			chat('/%s Next Up @ 75%% - Ulfaltering Bravado: Conal 10k damage split (line up behind tank)':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+		elseif not Ou_75 and Ou_Bravado_used then
+			Ou_75 = true
+			chat('/%s Next Up @ 60%% - Dancing Fullers: 2500 additive damage per player/pet hit dealt to each within 10\' AoE':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+		elseif not Ou_60 and Ou_Fullers_used then
+			Ou_60 = true
+			chat('/%s Next Up @ 45%% - Zero Hour: Throat-Stab, hate reset within 10\'(?) AoE':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+		elseif not Ou_45 and Ou_Zero_Hour_used then
+			Ou_45 = true
+			chat('/%s Next Up @ 30%% - Target: Switch hate after Target lands or 3min Terror':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+		elseif not Ou_30 and Ou_Target_used then
+			Ou_30 = true
+			coroutine.sleep(2)
+			chat('/%s Next Up @ 15%% - Gardez: Short Terror within 20\' AoE':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+		elseif not Ou_15 and Ou_Gardez_used then
+			Ou_15 = true
+			chat('/%s Next Up @ 10%% - Prophylaxis: HP recover to 22%%, 30 seconds to kill or complete battle reset':format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+		end
+
+		if Ou_100 and Ou.hpp == 100 then
+			resetOu()
+
+		end
+	end
+end
+
+function checkForCharm()
+	local justCharmed = {}
+
+	-- Loop through all party/alliance positions
+	for _, pos in ipairs(ally_pos) do
+		local member = windower.ffxi.get_mob_by_target(pos)
+		if member and member.charmed and not member.is_npc and not charmed[member.name] then
+			table.insert(justCharmed, member.name)
+			charmed[member.name] = true
+		elseif member and charmed[member.name] and not member.charmed then
+			--print('No longer charmed: '..member.name)
+			charmed[member.name] = nil
+		end
+	end
+
+	if #justCharmed > 0 then
+		chat(('/%s \r \r Charmed: '..table.concat(justCharmed, ", ")..'%s\r \r'):format(settings.chatmode,settings.chatmode == 'party' and ' <call14>' or ''))
+
+	end
+end
+
 windower.register_event('addon command', function(addcmd)
 
 	if addcmd == 'help' then
@@ -327,9 +430,9 @@ windower.register_event('addon command', function(addcmd)
 	elseif addcmd == 'list' or addcmd == 'lists' then
 		windower.add_to_chat(220,'[Callouts] '..('Current callouts:'):color(200)..'')
 		windower.add_to_chat(36,'   General'..(' - Charm'):color(8)..'')
-		windower.add_to_chat(36,'   Odyssey'..(' - Hate resets, full dispels, Bumba 1-HRs (beta), Kalunga/Ngai fetters'):color(8)..'')
+		windower.add_to_chat(36,'   Odyssey'..(' - Hate resets, full dispels, Bumba 1-HRs, Kalunga/Ngai fetters'):color(8)..'')
 		windower.add_to_chat(36,'   Dyna-D'..(' - Halphas hate reset'):color(8)..'')
-		windower.add_to_chat(36,'   Omen'..(' - Scales, Pain Sync, Prophylaxis, Target'):color(8)..'')
+		windower.add_to_chat(36,'   Omen'..(' - Scales, Pain Sync, Target, Ou abilities'):color(8)..'')
 		windower.add_to_chat(36,'   Vagary'..(' - Perfidien/Plouton pop, weaknesses'):color(8)..'')
 		windower.add_to_chat(36,'   Sortie'..(' - Elements/weaknesses, hate resets'):color(8)..'')
 		windower.add_to_chat(36,'   Ambuscade'..(' - Bozzetto Autarch Mijin Gakure'):color(8)..'')
@@ -340,6 +443,17 @@ windower.register_event('addon command', function(addcmd)
 
 	else
 		windower.add_to_chat(220,'[Callouts] '..('Unknown command. Type \'//co help\' for list of commands.'):color(8)..'')
+
 	end
-	
+end)
+
+windower.register_event('prerender', function()
+	local zone = windower.ffxi.get_info() and windower.ffxi.get_info().zone
+	if zone and zone == 292 then -- Reisenjima Henge
+		OuCallouts()
+	end
+	if os.time() > Heartbeat then
+		Heartbeat = os.time()
+		checkForCharm()
+	end
 end)
