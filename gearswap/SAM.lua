cb1d998e8e726e39a20eac86c4e99c3602d99276
Key <101156258+iLVL-Key@users.noreply.github.com>
2024-07-22 21:15:27 -0500

    Version 14.1.1

--- gearswap/SAM.lua
+++ gearswap/SAM.lua
@@ -638,7 +638,7 @@ end
 
 
 
-FileVersion = '14.1'
+FileVersion = '14.1.1'
 
 -------------------------------------------
 --             AREA MAPPING              --
@@ -1170,8 +1170,8 @@ end
 -------------------------------------------
 
 send_command('alias mode gs c Mode') --creates the Mode alias
+send_command('alias wc gs c WC') --creates the Weapon Cycle alias
 send_command('alias hud gs c HUD') --creates the HUD alias
-send_command('alias dt gs c DT') --creates the DT Override alias
 send_command('bind '..ModeBind..' gs c Mode') --creates the gear mode keyboard shortcut
 send_command('bind '..WCBind..' gs c WC') --creates the Weapon Cycle keyboard shortcut
 
@@ -3535,6 +3535,9 @@ windower.register_event('action',function(act)
 	end
 end)
 
+--set the Weapons at load
+send_command('gs c WC')
+
 -------------------------------------------
 --             FILE UNLOAD               --
 -------------------------------------------
@@ -3576,8 +3579,8 @@ function file_unload()
 	hud_abil05:destroy()
 	hud_abil06:destroy()
 	send_command('unalias mode')
+	send_command('unalias wc')
 	send_command('unalias hud')
-	send_command('unalias dt')
 	send_command('unbind '..ModeBind)
 	send_command('unbind '..WCBind)
 
