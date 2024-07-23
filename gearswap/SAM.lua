32985e31043d36102c3b8d88661ea72ae31660b6
Key <101156258+iLVL-Key@users.noreply.github.com>
2024-07-23 18:33:22 -0500

    Version 14.1.2

--- gearswap/SAM.lua
+++ gearswap/SAM.lua
@@ -638,7 +638,7 @@ end
 
 
 
-FileVersion = '14.1.1'
+FileVersion = '14.1.2'
 
 -------------------------------------------
 --             AREA MAPPING              --
@@ -3536,7 +3536,7 @@ windower.register_event('action',function(act)
 end)
 
 --set the Weapons at load
-send_command('gs c WC')
+send_command('wait 1;gs c WC')
 
 -------------------------------------------
 --             FILE UNLOAD               --
