963114eba0055cb015a79031ba525eab571c82db
Key <101156258+iLVL-Key@users.noreply.github.com>
2024-09-01 16:23:11 -0500

    Version 14.5.1

--- gearswap/SAM.lua
+++ gearswap/SAM.lua
@@ -673,7 +673,7 @@ end
 
 
 
-FileVersion = '14.5'
+FileVersion = '14.5.1'
 
 -------------------------------------------
 --             AREA MAPPING              --
@@ -2227,7 +2227,7 @@ function precast(spell)
 		equip(sets.hwater)
 	elseif spell.action_type == 'Ranged Attack' then
 		equip(sets.snapshot)
-	elseif not spell.action_type == 'Item' or spell.action_type == 'Ability' then
+	elseif not (spell.action_type == 'Item' or spell.action_type == 'Ability') then
 		equip(sets.fastcast)
 	end
 end
