b98e2e95cd9f8ff993b831dd2b8f821f6b207a99
Key <101156258+iLVL-Key@users.noreply.github.com>
2024-09-21 14:06:18 -0500

    Version 14.4.6
    Sublimation.recast subjob restricted error

--- gearswap/PLD.lua
+++ gearswap/PLD.lua
@@ -810,7 +810,7 @@ end
 
 
 
-FileVersion = '14.4.5'
+FileVersion = '14.4.6'
 
 -------------------------------------------
 --             AREA MAPPING              --
@@ -2491,7 +2491,7 @@ function aftercast(spell)
 		send_command('wait 3; input /ja Majesty <me>')
 	end
 	choose_set()
-	if AutoSubCharge and player.sub_job == 'SCH' and Sublimation.recast < 2 and not (buffactive['amnesia'] or buffactive['Sublimation: Activated'] or buffactive['Sublimation: Complete'] or buffactive['Refresh'] or buffactive['Invisible']) then
+	if AutoSubCharge and player.sub_job == 'SCH' and Sublimation.recast and Sublimation.recast < 2 and not (buffactive['amnesia'] or buffactive['Sublimation: Activated'] or buffactive['Sublimation: Complete'] or buffactive['Refresh'] or buffactive['Invisible']) then
 		if not double_sublimation_fix then
 			double_sublimation_fix = true --prevents this from running through here a second time after being cast again below
 			if spell.type == 'WeaponSkill' or spell.action_type == 'Magic' then
@@ -2517,7 +2517,7 @@ windower.register_event('status change', function(status)
 		windower.send_command('gs c ShowHUD')
 	end
 	choose_set()
-	if AutoSubCharge and player.sub_job == 'SCH' and status == 0 and Sublimation.recast < 2 and not (buffactive['amnesia'] or buffactive['Sublimation: Activated'] or buffactive['Sublimation: Complete'] or buffactive['Refresh'] or buffactive['Invisible']) then
+	if AutoSubCharge and player.sub_job == 'SCH' and status == 0 and Sublimation.recast and Sublimation.recast < 2 and not (buffactive['amnesia'] or buffactive['Sublimation: Activated'] or buffactive['Sublimation: Complete'] or buffactive['Refresh'] or buffactive['Invisible']) then
 		if not double_sublimation_fix then
 			double_sublimation_fix = true --prevents this from running a second time (as an aftercast above) after being run here
 			send_command('input /ja Sublimation <me>;wait 1;gs c double_sublimation_fix')
