de811a9b7796a4ef4ac0d7cc22675c75889aa73c
Key <101156258+iLVL-Key@users.noreply.github.com>
2025-09-20 10:45:19 -0500

    Version 4.4.1

--- addons/Bars/Bars.lua
+++ addons/Bars/Bars.lua
@@ -25,7 +25,7 @@
 --SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 _addon.name = 'Bars'
-_addon.version = '4.4'
+_addon.version = '4.4.1'
 _addon.author = 'Key (Keylesta@Valefor)'
 _addon.commands = {'bars'}
 
