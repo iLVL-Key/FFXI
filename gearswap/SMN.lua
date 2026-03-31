71025fc2802b825fb5d23eea1f4eb8c93d32ada6
Key <101156258+iLVL-Key@users.noreply.github.com>
2026-03-31 13:23:03 -0500

    Version 14.1

--- gearswap/SMN.lua
+++ gearswap/SMN.lua
@@ -161,6 +161,7 @@ WarningRepeat		=	5		--Maximum number of times the Warning Sound will repeat, onc
 RRReminderTimer		=	3600	--Delay in seconds between checks to see if Reraise is up (300 is 5 minutes).
 NotiDelay			=	6		--Delay in seconds before certain notifications will automatically clear.
 PollingRate			=	5		--Times per second to check for various conditions (debuffs, recasts, etc). Higher rates use more CPU.
+MovementCastDelay	=	0.5		--[#]  Delays casting UP TO this amount in seconds when coming to a stop from moving to help prevent interruption. Set to 0 to disable.
 AddCommas			=	true	--[true/false]  Adds commas to damage numbers.
 
 -------------------------------------------
@@ -623,7 +624,7 @@ end
 
 
 
-FileVersion = '14.0'
+FileVersion = '14.1'
 
 -------------------------------------------
 --            AVATAR MAPPING             --
@@ -709,6 +710,7 @@ NotiLowMPToggle = false --start with the toggle off for the Low MP Notification 
 RRRCountdown = RRReminderTimer
 HUDposYLine1 = HUDposY
 last_poll = 0 --keeps the timing for things that happen at the polling rate
+last_captured_poll = 0 --keeps the timing for the MovementCastDelay polling rate (0.1 seconds)
 last_second = 0 --keeps the timing for things that happen every second
 GreetingDelay = 6 --delay to display greeting and file version info
 Zoning = false --flips automatically to hide the HUD while zoning
@@ -741,6 +743,8 @@ transport_lock_timestamp = 0
 player_x = nil
 player_y = nil
 moving = false
+captured = {}
+captured_spell_toggle = false
 
 local play_sound = windower.play_sound
 local addon_path = windower.addon_path
@@ -1808,6 +1812,8 @@ function self_command(command)
 		double_release_fix = false
 	elseif command == "double_sublimation_fix" then
 		double_sublimation_fix = false
+	elseif command == "resetCapturedToggle" then
+		captured_spell_toggle = false
 	end
 end
 
@@ -1851,6 +1857,27 @@ end
 -------------------------------------------
 
 function precast(spell)
+
+	local prefixes = {
+		["/magic"] = true,
+		["/song"] = true,
+		["/ninjutsu"] = true,
+		['/trust'] = true,
+		['/item'] = true,
+		['/range'] = true,
+	}
+	if MovementCastDelay ~= 0 and not captured_spell_toggle and prefixes[spell.prefix] and moving then
+		captured_spell_toggle = true
+		if spell.prefix == "/range" then
+			captured.spell = "/range "..spell.target.raw
+			captured.timestamp = os.clock() + MovementCastDelay
+		else
+			captured.spell = spell.prefix.." \""..spell.name.."\" "..spell.target.raw
+			captured.timestamp = os.clock() + MovementCastDelay
+		end
+		cancel_spell()
+		return
+	end
 
 	local transport_spells = {
 		['Teleport-Holla'] = true, ['Teleport-Dem'] = true, ['Teleport-Mea'] = true, ['Teleport-Altep'] = true, ['Teleport-Yhoat'] = true, ['Teleport-Vahzl'] = true, ['Recall-Jugner'] = true, ['Recall-Meriph'] = true, ['Recall-Pashh'] = true, ['Warp'] = true, ['Warp II'] = true, ['Retrace'] = true, ['Escape'] = true
@@ -2313,9 +2340,25 @@ windower.register_event('prerender', function()
 		return
 	end
 
-	--Polling rate
 	local current_time = os.clock()
 
+	--Check for captured spells (to delay them while coming to a stop from moving)
+	if captured.timestamp and current_time > last_captured_poll + 0.1 then
+		last_captured_poll = current_time
+		if captured.timestamp > current_time then
+			if not moving then
+				send_command('wait 0.05;input '..captured.spell)
+				captured = {}
+				send_command('wait 1;gs c resetCapturedToggle')
+			end
+		else
+			send_command('wait 0.05;input '..captured.spell)
+				captured = {}
+			send_command('wait 1;gs c resetCapturedToggle')
+		end
+	end
+
+	--Polling rate
 	if current_time - last_poll >= 1 / PollingRate then
 
 		last_poll = current_time
@@ -3309,8 +3352,6 @@ windower.register_event('action',function(act)
 
 	local ata = act.targets[1].actions[1]
 	local msg = ata.message
-	local target_id = act.targets[1].id
-	local clock = os.clock()
 
 	--Check if a monsters attack hits the player
 	if DangerMode == "Auto" and (not DangerPTOnly or playerIsInAPartyOrAlliance()) and isMonster(act.actor_id) then
