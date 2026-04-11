77eadb932f1bde1914c6721aa0470f524de35375
Key <101156258+iLVL-Key@users.noreply.github.com>
2026-04-11 12:24:03 -0500

    Version 15.2.1

--- gearswap/PLD.lua
+++ gearswap/PLD.lua
@@ -191,7 +191,8 @@ AutSntThreshold		=	500		--If your HP goes below this number, Sentinel will activ
 RRReminderTimer		=	3600	--Delay in seconds between checks to see if Reraise is up (300 is 5 minutes).
 NotiDelay			=	6		--Delay in seconds before certain notifications will automatically clear.
 PollingRate			=	5		--Times per second to check for various conditions (debuffs, recasts, etc). Higher rates use more CPU.
-MovementCastDelay	=	0.5		--[#]  Delays casting UP TO this amount in seconds when coming to a stop from moving to help prevent interruption. Set to 0 to disable.
+MoveCastWindow		=	1		--[#]  Window in seconds to wait to come a stop from moving before cassting a spell to help prevent interruption. Set to 0 to disable.
+MoveCastDelay		=	0.25	--[#]  Delay in seconds to wait AFTER coming to a stop before casting the pending spell to help prevent interruption.
 AddCommas			=	true	--[true/false]  Adds commas to damage numbers.
 
 -------------------------------------------
@@ -805,8 +806,8 @@ end
 
 
 
-FileVersion = '15.2'
- 
+FileVersion = '15.2.1'
+
 -------------------------------------------
 --             AREA MAPPING              --
 -------------------------------------------
@@ -856,7 +857,7 @@ NotiLowMPToggle = false --start with the toggle off for the Low MP Notification 
 RRRCountdown = RRReminderTimer
 HUDposYLine1 = HUDposY
 last_poll = 0 --keeps the timing for things that happen at the polling rate
-last_captured_poll = 0 --keeps the timing for the MovementCastDelay polling rate (0.1 seconds)
+last_captured_poll = 0 --keeps the timing for the MoveCastWindow polling rate (0.1 seconds)
 last_second = 0 --keeps the timing for things that happen every second
 GreetingDelay = 6 --delay to display greeting and file version info
 Zoning = false --flips automatically to hide the HUD while zoning
@@ -2106,7 +2107,8 @@ function choose_set()
 		else
 			local auto_combat = (Mode == 'Auto' and player_in_combat)
 			local base_set = (auto_combat or Mode == "Kite" or Mode == "Tank") and (TankMDTTriggerFound() and sets.tank_mdt or sets.tank) or sets.idle
-			local autorun = windower.ffxi.get_player().autorun
+			local get_player = windower.ffxi.get_player()
+			local autorun = get_player and get_player.autorun
 			local auto_movement_speed = AutoMvmntSpeed and moving
 			local movement_speed = (auto_combat or Mode == "Kite" or auto_movement_speed or autorun) and sets.movement_speed or nil
 			equip(set_combine(base_set, max_hp, low_hp, movement_speed, {main=pair[1],sub=pair[2]}))
@@ -2129,14 +2131,14 @@ function precast(spell)
 		['/item'] = true,
 		['/range'] = true,
 	}
-	if MovementCastDelay ~= 0 and not captured_spell_toggle and prefixes[spell.prefix] and moving then
+	if MoveCastWindow ~= 0 and not captured_spell_toggle and prefixes[spell.prefix] and moving then
 		captured_spell_toggle = true
 		if spell.prefix == "/range" then
 			captured.spell = "/range "..spell.target.raw
-			captured.timestamp = os.clock() + MovementCastDelay
+			captured.timestamp = os.clock() + MoveCastWindow
 		else
 			captured.spell = spell.prefix.." \""..spell.name.."\" "..spell.target.raw
-			captured.timestamp = os.clock() + MovementCastDelay
+			captured.timestamp = os.clock() + MoveCastWindow
 		end
 		cancel_spell()
 		return
@@ -2668,14 +2670,14 @@ windower.register_event('prerender', function()
 		last_captured_poll = current_time
 		if captured.timestamp > current_time then
 			if not moving then
-				send_command('wait 0.05;input '..captured.spell)
+				send_command('wait '..MoveCastDelay..';input '..captured.spell)
 				captured = {}
-				send_command('wait 1;gs c resetCapturedToggle')
+				send_command('wait '..(MoveCastDelay + 1)..';gs c resetCapturedToggle')
 			end
 		else
-			send_command('wait 0.05;input '..captured.spell)
+			send_command('wait '..MoveCastDelay..';input '..captured.spell)
 				captured = {}
-			send_command('wait 1;gs c resetCapturedToggle')
+			send_command('wait '..(MoveCastDelay + 1)..';gs c resetCapturedToggle')
 		end
 	end
 
@@ -2704,7 +2706,7 @@ windower.register_event('prerender', function()
 			if get_player then
 				--Player has started moving
 				if player_x ~= get_player.x or player_y ~= get_player.y then
-					if not moving then
+					if not moving and player.status == "Idle" then
 						moving = true
 						choose_set()
 					end
@@ -3106,6 +3108,7 @@ windower.register_event('prerender', function()
 				if not player_in_combat then
 					player_in_combat = true
 					choose_set()
+					setNotification()
 					-- if player.status == "Idle" then
 					-- 	hud_mode_shdw:text('Mode: Auto (Kite)')
 					-- 	hud_mode:text('Mode: Auto (Kite)')
@@ -3122,6 +3125,7 @@ windower.register_event('prerender', function()
 				if player_in_combat then
 					player_in_combat = false
 					choose_set()
+					setNotification()
 					-- hud_mode_shdw:text('Mode: Auto (Neutral)')
 					-- hud_mode:text('Mode: Auto (Neutral)')
 					-- local c = color.Neutral
@@ -3142,7 +3146,7 @@ windower.register_event('prerender', function()
 			hud_noti:color(255,50,50)
 			NotiCountdown = NotiDelay
 			send_command('wait 30;gs c NotiLowMPToggle') --wait 30 sec then turns the toggle back off
-		elseif notifications.LowMP and player and player.mpp > 20 and LowMP then
+		elseif notifications.LowMP and player and player.mpp > 20 and lowMP then
 			lowMP = false
 			setNotification()
 		end
