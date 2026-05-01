3736dc310aa8709f493aeb1148c5d2a6233e192b
Key <101156258+iLVL-Key@users.noreply.github.com>
2026-05-01 12:55:39 -0500

    Version 2.2.2

--- gearswap/COR.lua
+++ gearswap/COR.lua
@@ -1064,7 +1064,7 @@ end
 
 
 
-FileVersion = '2.2.1'
+FileVersion = '2.2.2'
 
 -------------------------------------------
 --             AREA MAPPING              --
@@ -1887,7 +1887,7 @@ end
 
 --Check if we have access to dual wield
 local function hasDualWield()
-	if player.sub_job == 'NIN' or player.sub_job == 'DNC' then
+	if (player.sub_job == 'NIN' and player.sub_job_level >= 10) or (player.sub_job == 'DNC' and player.sub_job_level >= 20) then
 		return true
 	else
 		return false
@@ -4706,10 +4706,10 @@ windower.register_event('prerender', function()
 			NotiCountdown = -1
 		end
 
-		if notifications.Doom and buffactive['doom'] then
+		if notifications.Doom and buffactive['doom'] and Alive then
 			flash('Debuffs')
 		end
-		if (notifications.Doom and buffactive['doom']) or (notifications.LowHP and LowHP and Alive and not (buffactive['weakness'] or TownZones[world.area] or windower.ffxi.get_info().mog_house)) and AlertSounds and WarningCountdown > 0 then
+		if (notifications.Doom and buffactive['doom'] and Alive) or (notifications.LowHP and LowHP and Alive and not (buffactive['weakness'] or TownZones[world.area] or windower.ffxi.get_info().mog_house)) and AlertSounds and WarningCountdown > 0 then
 			WarningCountdown = WarningCountdown - 1
 			play_sound(Notification_Danger)
 		end
