e0de2f209afe2a94ec8c6071d4a44a23294cce58
Key <101156258+iLVL-Key@users.noreply.github.com>
2026-05-01 12:51:02 -0500

    Version 20.1.3

--- gearswap/BLU.lua
+++ gearswap/BLU.lua
@@ -761,7 +761,7 @@ end
 
 
 
-FileVersion = '20.1.2'
+FileVersion = '20.1.3'
 
 -------------------------------------------
 --            SPELL MAPPING              --
@@ -3971,10 +3971,10 @@ windower.register_event('prerender', function()
 			NotiCountdown = -1
 		end
 
-		if notifications.Doom and buffactive['doom'] then 
+		if notifications.Doom and buffactive['doom'] and Alive then 
 			flash('Debuffs')
 		end
-		if (notifications.Doom and buffactive['doom']) or (notifications.LowHP and LowHP and Alive and not (buffactive['weakness'] or TownZones[world.area] or windower.ffxi.get_info().mog_house)) and AlertSounds and WarningCountdown > 0 then
+		if (notifications.Doom and buffactive['doom'] and Alive) or (notifications.LowHP and LowHP and Alive and not (buffactive['weakness'] or TownZones[world.area] or windower.ffxi.get_info().mog_house)) and AlertSounds and WarningCountdown > 0 then
 			WarningCountdown = WarningCountdown - 1
 			play_sound(Notification_Danger)
 		end
