928013455c4c1a1db6eb8a4b794bf9b25bd8f03c
Key <101156258+iLVL-Key@users.noreply.github.com>
2026-05-09 17:05:20 -0500

    Version 3.1.4

--- gearswap/BRD.lua
+++ gearswap/BRD.lua
@@ -920,7 +920,7 @@ end
 
 
 
-FileVersion = '3.1.3'
+FileVersion = '3.1.4'
 
 -------------------------------------------
 --             AREA MAPPING              --
@@ -3081,10 +3081,10 @@ function precast(spell)
 		captured_spell_toggle = true
 		if spell.prefix == "/range" then
 			captured.spell = "/range "..spell.target.raw
-			captured.timestamp = os.clock() + MoveCastWindow
+			captured.timestamp = os.time() + MoveCastWindow
 		else
 			captured.spell = spell.prefix.." \""..spell.name.."\" "..spell.target.raw
-			captured.timestamp = os.clock() + MoveCastWindow
+			captured.timestamp = os.time() + MoveCastWindow
 		end
 		cancel_spell()
 		return
@@ -3583,7 +3583,7 @@ windower.register_event('prerender', function()
 		return
 	end
 
-	local current_time = os.clock()
+	local current_time = os.time()
 
 	--Check for captured spells (to delay them while coming to a stop from moving)
 	if captured.timestamp and current_time > last_captured_poll + 0.1 then
