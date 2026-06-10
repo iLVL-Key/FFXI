ce36cc4040268cb7ce25be43807030b93e1681ad
Key <101156258+iLVL-Key@users.noreply.github.com>
2026-06-09 19:12:40 -0500

    Version 10.3

--- gearswap/RUN.lua
+++ gearswap/RUN.lua
@@ -155,6 +155,7 @@ AlertSounds		=	true	--[true/false]	Plays a sound on alerts.
 UseEcho			=	'R'		--[E/R/Off]		Automatically uses an (E)cho Drop or (R)emedy instead of spell when you are silenced.
 AutoGearCheck	=	true	--[true/false]	Automatically checks and equips appropriate gear set on player movement.
 AutoMvmntSpeed	=	true	--[true/false]	Automatically equips Movement Speed set on player movement when idle and not in combat.
+AutoPhalanxSet	=	true	--[true/false]	Automatically equips Phalanx gear set when another player casts Phalanx II or Accession Phalanx on you.
 AutoStance		=	true	--[true/false]	Automatically activates and keeps Hasso/Seigan Stances active.
 
 -- Heads Up Display --
@@ -550,12 +551,19 @@ sets.enhancing = {
 }
 
 -- Phalanx (Phalanx, Enhancing Magic Duration)
+-- NOTE: Phalanx tiers are at 300/329/358/386/415/443/472/500 Enhancing Magic Skill, anything in between or higher than 500 is wasted
 sets.phalanx = set_combine(sets.enhancing, {
 	head="Fu. Bandeau +4",
-	body="Herculean Vest",
+	body={ name="Herculean Vest", augments={'Pet: INT+5','Phalanx +3',}},
 	hands={ name="Herculean Gloves", augments={'INT+3','Crit.hit rate+2','Phalanx +3','Accuracy+12 Attack+12',}},
-	legs="Herculean Trousers",
+	legs={ name="Herculean Trousers", augments={'DEX+7','"Mag.Atk.Bns."+1','Phalanx +4','Accuracy+17 Attack+17','Mag. Acc.+2 "Mag.Atk.Bns."+2',}},
 	feet={ name="Herculean Boots", augments={'Pet: STR+9','Accuracy+18','Phalanx +3','Accuracy+19 Attack+19',}},
+	waist="Plat. Mog. Belt",
+	left_ear="Alabaster Earring",
+	right_ear="Erilaz Earring +2",
+	left_ring="Murky Ring",
+	right_ring="Defending Ring",
+	back="Moonlight Cape",
 })
 
 -- Refresh Spell (Refresh potency, Enhancing Magic Duration)
@@ -747,7 +755,7 @@ end
 
 
 
-FileVersion = '10.2.6'
+FileVersion = '10.3'
 
 -------------------------------------------
 --             AREA MAPPING              --
@@ -844,6 +852,7 @@ player_y = nil
 moving = false
 captured = {}
 captured_spell_toggle = false
+active_accession = {}
 
 local play_sound = windower.play_sound
 local addon_path = windower.addon_path
@@ -1920,6 +1929,18 @@ local function setNotification()
 
 end
 
+local party_positions = {'p1', 'p2', 'p3', 'p4', 'p5'}
+--Check that the given player is in the party
+local function isPlayerInParty(player_id)
+	for _, pos in ipairs(party_positions) do
+		local member = windower.ffxi.get_mob_by_target(pos)
+		if member and member.id == player_id then
+			return true
+		end
+	end
+	return false
+end
+
 -------------------------------------------
 --            SELF COMMANDS              --
 -------------------------------------------
@@ -2904,12 +2925,12 @@ windower.register_event('prerender', function()
 		return
 	end
 
-	local current_time = os.clock()
+	local clock = os.clock()
 
 	--Check for captured spells (to delay them while coming to a stop from moving)
-	if captured.timestamp and current_time > last_captured_poll + 0.1 then
-		last_captured_poll = current_time
-		if captured.timestamp > current_time then
+	if captured.timestamp and clock > last_captured_poll + 0.1 then
+		last_captured_poll = clock
+		if captured.timestamp > clock then
 			if not moving then
 				send_command('wait '..MoveCastDelay..';input '..captured.spell)
 				captured = {}
@@ -2923,9 +2944,9 @@ windower.register_event('prerender', function()
 	end
 
 	--Polling rate
-	if current_time - last_poll >= 1 / PollingRate then
+	if clock - last_poll >= 1 / PollingRate then
 
-		last_poll = current_time
+		last_poll = clock
 
 		--Zoning: hide HUD
 		local pos = windower.ffxi.get_position()
@@ -2954,6 +2975,13 @@ windower.register_event('prerender', function()
 				end
 				player_x = get_player.x
 				player_y = get_player.y
+			end
+		end
+
+		--Clear expired Accession timers
+		for id, timestamp in pairs(active_accession) do
+			if timestamp <= clock then
+				active_accession[id] = nil
 			end
 		end
 
@@ -4083,9 +4111,9 @@ windower.register_event('prerender', function()
 	end
 
 	--1 second heartbeat
-	if current_time - last_second >= 1 then
+	if clock - last_second >= 1 then
 
-		last_second = current_time
+		last_second = clock
 
 		if Rune1Timer > 151 then
 			Rune1Timer = Rune1Timer - 1
@@ -4319,6 +4347,9 @@ end
 -------------------------------------------
 
 windower.register_event('incoming text',function(org)
+
+	if not org then return end
+
 	if org:find('wishes to trade with you') then
 		if AlertSounds then
 			play_sound(Notification_Good)
@@ -4401,15 +4432,38 @@ windower.register_event('incoming text',function(org)
 end)
 
 -------------------------------------------
---         DAMAGE NOTIFICATIONS          --
+--             ACTION EVENTS             --
 -------------------------------------------
 
 windower.register_event('action',function(act)
 
-	if not notifications.Damage then return end
-
 	local ata = act.targets[1].actions[1]
 	local msg = ata.message
+	local target_id = act.targets[1].id
+
+	--Track Phalanx things our party is doing
+	if AutoPhalanxSet and isPlayerInParty(act.actor_id) then
+		if act.category == 4 then --Completion of spell
+			if act.param == 106 and active_accession[act.actor_id] then --Phalanx + Accession active
+				choose_set()
+				active_accession[act.actor_id] = nil --Remove active Accession after it's been used
+			elseif act.param == 107 and target_id == player.id then --Phalanx II on player
+				choose_set()
+			end
+		elseif act.category == 6 and msg == 100 then --Uses ability
+			if act.param == 218 then --Accession
+				active_accession = { --Player now has Accession active
+					[act.actor_id] = os.clock() + 60, --Accession wears off after 60 seconds
+				}
+			end
+		elseif act.category == 8 then --Start of spell
+			if (ata.param == 106 and active_accession[act.actor_id]) or (ata.param == 107 and target_id == player.id) then --Phalanx + Accession active or Phalanx II on player
+				equip(sets.phalanx)
+			end
+		end
+	end
+
+	if not notifications.Damage then return end
 
 	--Weapon Skills and Skillchains:
 	if act.category == 3 and act.actor_id == player.id then
