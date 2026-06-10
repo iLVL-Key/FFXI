649a18032d10844cd7d0f15695131890079e7d04
Key <101156258+iLVL-Key@users.noreply.github.com>
2026-06-09 19:20:23 -0500

    Version 10.4

--- gearswap/WAR.lua
+++ gearswap/WAR.lua
@@ -116,6 +116,7 @@ AlertSounds		=	true	--[true/false]	Plays a sound on alerts.
 UseEcho			=	'R'		--[E/R/Off]		Automatically uses an (E)cho Drop or (R)emedy instead of spell when you are silenced.
 AutoGearCheck	=	true	--[true/false]	Automatically checks and equips appropriate gear set on player movement.
 AutoMvmntSpeed	=	true	--[true/false]	Automatically equips Movement Speed set on player movement when idle.
+AutoPhalanxSet	=	true	--[true/false]	Automatically equips Phalanx gear set when another player casts Phalanx II or Accession Phalanx on you.
 AutoStance		=	true	--[true/false]	Automatically activates and keeps Hasso/Seigan Stances active.
 AutoSave		=	true	--[true/false]	Attempts to use High Jump when your HP gets critically low.
 							--				NOTE: Will not activate while in Seigan Stance.
@@ -525,7 +526,7 @@ sets.idle = {
 	left_ear="Alabaster Earring",
 	right_ear="Boii Earring +2",
 	left_ring="Defending Ring",
-	right_ring="Karieyh Ring +1",
+	right_ring="Shadow Ring",
 	back="Null Shawl",
 }
 
@@ -533,7 +534,7 @@ sets.idle = {
 -- Equipped while in town, and automatically while moving outside of town if the AutoMvmntSpeed option is enabled.
 -- NOTE: If AutoMvmntSpeed is disabled, be sure to include your movement speed gear in the Idle set above.
 sets.movement_speed = {
-	feet="Hermes' Sandals",
+	right_ring="Shneddick Ring +1",
 }
 
 -- Oh Shit
@@ -564,7 +565,7 @@ sets.weapon_skill = {
 	left_ear="Moonshade Earring",
 	right_ear="Thrud Earring",
 	left_ring="Cornelia's Ring",
-	right_ring="Karieyh Ring +1",
+	right_ring="Epaminondas's Ring",
 	back={ name="Cichol's Mantle", augments={'STR+20','Accuracy+20 Attack+20','STR+10','Weapon skill damage +10%','Phys. dmg. taken-10%',}},
 }
 
@@ -574,7 +575,7 @@ sets.ws_accuracy = {
 	neck="Fotia Gorget",
 	waist="Fotia Belt",
 	left_ring="Cornelia's Ring",
-	right_ring="Karieyh Ring +1",
+	right_ring="Chirich Ring +1",
 }
 
 -- Armor Break (Magic Accuracy)
@@ -815,6 +816,22 @@ sets.fast_cast = {
 	right_ring="Prolix Ring",
 }
 
+-- Phalanx (Phalanx+, Enhancing Magic+, Enhancing Magic Duration)
+-- NOTE: Phalanx tiers are at 300/329/358/386/415/443/472/500 Enhancing Magic Skill, anything in between or higher than 500 is wasted
+-- NOTE: This set is also used when another player casts a Phalanx II or Accessioned Phalanx on you.
+sets.phalanx = {
+	head={ name="Odyssean Helm", augments={'AGI+15','Accuracy+8','Phalanx +4','Accuracy+10 Attack+10','Mag. Acc.+20 "Mag.Atk.Bns."+20',}},
+	body="Odyssean Chestplate",
+	hands="Souv. Handsch. +1",
+	legs="Sakpata's Cuisses",
+	feet="Souveran Schuhs +1",
+	waist="Plat. Mog. Belt",
+	left_ear="Alabaster Earring",
+	left_ring="Murky Ring",
+	right_ring="Defending Ring",
+	back="Moonlight Cape",
+}
+
 -- Snapshot
 sets.snapshot = {
 
@@ -955,7 +972,7 @@ end
 
 
 
-FileVersion = '10.3.4'
+FileVersion = '10.4'
 
 -------------------------------------------
 --             AREA MAPPING              --
@@ -1046,6 +1063,7 @@ player_y = nil
 moving = false
 captured = {}
 captured_spell_toggle = false
+active_accession = {}
 
 local play_sound = windower.play_sound
 local addon_path = windower.addon_path
@@ -1817,7 +1835,6 @@ local function getHUDAbils()
 	hud_abil06:text(abil06)
 
 end
-
 getHUDAbils()
 
 local function formatAMTime(input)
@@ -2014,6 +2031,18 @@ local function setNotification()
 		hud_noti:color(255,255,255)
 	end
 
+end
+
+local party_positions = {'p1', 'p2', 'p3', 'p4', 'p5'}
+--Check that the given player is in the party
+local function isPlayerInParty(player_id)
+	for _, pos in ipairs(party_positions) do
+		local member = windower.ffxi.get_mob_by_target(pos)
+		if member and member.id == player_id then
+			return true
+		end
+	end
+	return false
 end
 
 -------------------------------------------
@@ -2690,12 +2719,12 @@ windower.register_event('prerender', function()
 		return
 	end
 
-	local current_time = os.clock()
+	local clock = os.clock()
 
 	--Check for captured spells (to delay them while coming to a stop from moving)
-	if captured.timestamp and current_time > last_captured_poll + 0.1 then
-		last_captured_poll = current_time
-		if captured.timestamp > current_time then
+	if captured.timestamp and clock > last_captured_poll + 0.1 then
+		last_captured_poll = clock
+		if captured.timestamp > clock then
 			if not moving then
 				send_command('wait '..MoveCastDelay..';input '..captured.spell)
 				captured = {}
@@ -2709,9 +2738,9 @@ windower.register_event('prerender', function()
 	end
 
 	--Polling rate
-	if current_time - last_poll >= 1 / PollingRate then
+	if clock - last_poll >= 1 / PollingRate then
 
-		last_poll = current_time
+		last_poll = clock
 
 		--Attempt an AutoSave
 		if AutoSave and player.sub_job == 'DRG' and player.hp <= AutoSaveThreshold and Alive and not (buffactive['Weakness'] or buffactive['amnesia'] or buffactive['impairment'] or buffactive['terror'] or buffactive['petrification'] or buffactive['sleep'] or Stance == 'Seigan') and player.status == "Engaged" and not (TownZones[world.area] or windower.ffxi.get_info().mog_house) and not AutoSaveUsed then
@@ -2747,6 +2776,13 @@ windower.register_event('prerender', function()
 				end
 				player_x = get_player.x
 				player_y = get_player.y
+			end
+		end
+
+		--Clear expired Accession timers
+		for id, timestamp in pairs(active_accession) do
+			if timestamp <= clock then
+				active_accession[id] = nil
 			end
 		end
 
@@ -3632,9 +3668,9 @@ windower.register_event('prerender', function()
 	end
 
 	--1 second heartbeat
-	if current_time - last_second >= 1 then
+	if clock - last_second >= 1 then
 
-		last_second = current_time
+		last_second = clock
 
 		if (buffactive['Hasso'] or buffactive['Seigan']) then
 			StanceTimer = StanceTimer - 1
@@ -3771,6 +3807,9 @@ end
 -------------------------------------------
 
 windower.register_event('incoming text',function(org)
+
+	if not org then return end
+
 	if org:find('wishes to trade with you') then
 		if AlertSounds then
 			play_sound(Notification_Good)
@@ -3845,13 +3884,36 @@ windower.register_event('incoming text',function(org)
 end)
 
 -------------------------------------------
---         DAMAGE NOTIFICATIONS          --
+--             ACTION EVENTS             --
 -------------------------------------------
 
 windower.register_event('action',function(act)
 
 	local ata = act.targets[1].actions[1]
 	local msg = ata.message
+	local target_id = act.targets[1].id
+
+	--Track Phalanx things our party is doing
+	if AutoPhalanxSet and isPlayerInParty(act.actor_id) then
+		if act.category == 4 then --Completion of spell
+			if act.param == 106 and active_accession[act.actor_id] then --Phalanx + Accession active
+				choose_set()
+				active_accession[act.actor_id] = nil --Remove active Accession after it's been used
+			elseif act.param == 107 and target_id == player.id then --Phalanx II on player
+				choose_set()
+			end
+		elseif act.category == 6 and msg == 100 then --Uses ability
+			if act.param == 218 then --Accession
+				active_accession = { --Player now has Accession active
+					[act.actor_id] = os.clock() + 60, --Accession wears off after 60 seconds
+				}
+			end
+		elseif act.category == 8 then --Start of spell
+			if (ata.param == 106 and active_accession[act.actor_id]) or (ata.param == 107 and target_id == player.id) then --Phalanx + Accession active or Phalanx II on player
+				equip(sets.phalanx)
+			end
+		end
+	end
 	
 	--Weapon Skills and Skillchains:
 	if notifications.Damage and act.category == 3 and act.actor_id == player.id then
